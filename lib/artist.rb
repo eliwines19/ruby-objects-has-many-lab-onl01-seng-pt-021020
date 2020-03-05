fcb5fff714d03cbc9acace03a9e56bd9f628ecee
Elijah wines <elijahwines84@yahoo.com>
2020-03-05 15:17:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ class Artist
 
   attr_accessor :name, :artist
 
-  @count = 0
+  @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -16,6 +16,7 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def songs
@@ -25,9 +26,10 @@ class Artist
   def add_song_by_name(name)
     song = Song.new(name)
     add_song(song)
+    @@song_count += 1
   end
 
   def self.song_count
-    @songs.size
+    @@song_count
   end
 end
