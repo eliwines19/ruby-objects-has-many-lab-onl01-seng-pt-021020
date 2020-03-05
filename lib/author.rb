9c405ecf49b5542bd3e334ce668d74c418a8cf13
Elijah wines <elijahwines84@yahoo.com>
2020-03-05 15:42:59 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,10 +3,14 @@ class Author
   attr_accessor :name, :posts
 
   @@post_count = 0
-
+  
   def initialize(name)
     @name = name
     @posts = []
+  end
+
+  def posts
+    Post.all.select {|post| post.author = self}
   end
 
   def add_post(post)
